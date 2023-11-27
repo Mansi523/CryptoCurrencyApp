8e2d5bf947e860c229eac7563e4e7d39067e3e81
Mansi523 <kumarimansi523@gmail.com>
2023-11-27 00:22:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+      CryptoCurrency
+     </h1>
     </div>
   );
 }
 
 export default App;
+
+
+// echo "# CryptoCurrencyApp" >> README.md
+//   git init
+//   git add README.md
+//   git commit -m "first commit"
+//   git branch -M main
+//   git remote add origin https://github.com/Mansi523/CryptoCurrencyApp.git
+//   git push -u origin main
